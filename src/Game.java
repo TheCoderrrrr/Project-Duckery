d109dfe54e91a7f7b5babb8ede9eba317c138e1c
Kevin <kevin.jiang.835@gmail.com>
2024-08-29 23:06:03 -0400

    Main and Game added

---
+++ src/Game.java
@@ -0,0 +1,52 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Game extends BasicGameState {
+    private int id;
+    private static GameContainer gc;
+    private static StateBasedGame sbg;
+
+    public Game(int id)
+    {
+        this.id = id;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    @Override
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        this.gc = gc;
+        this.sbg = sbg;
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
+
+    }
+    public void enter(GameContainer gc, StateBasedGame sbg) throws SlickException
+    {
+
+    }
+    public void leave(GameContainer gc, StateBasedGame sbg)
+    {
+
+    }
+    public void keyPressed(int key, char c)
+    {
+
+    }
+    public void mousePressed(int button , int x, int y)
+    {
+
+    }
+}
