a836cbf709fe253c7bbc63462f9b4a41b651e48f
jessi <jessicabaek111@gmail.com>
2024-09-08 22:07:54 -0400

    saving stuff

--- src/game/Clipboard.java
+++ src/game/Clipboard.java
@@ -17,6 +17,15 @@ public class Clipboard {
     public void render(Graphics g) {
         g.setColor(Color.orange);
         g.fillRect(1100,70,700,900);
+
+        g.setColor(Color.magenta);
+        g.fillRect(1150,100, 100, 100);
+
+        g.setColor(Color.blue);
+        g.fillRect(1150, 450, 100, 100);
+
+        g.setColor(Color.darkGray);
+        g.fillRect(1150, 800, 100,100);
     }
 
     public void update() {
@@ -28,5 +37,22 @@ public class Clipboard {
     }
 
     public void mousePressed(int button, int x, int y) {
+
+        if(button == 0 && x >= 1150 && x <= 1250 && y >= 110 && y <= 198)
+        {
+            System.out.println("Duck hired!");
+        }
+
+        if(button == 0 && x >= 1153 && x <= 1245 && y >= 455 && y <= 545)
+        {
+            System.out.println("Researching products..");
+        }
+
+        if(button == 0 && x >= 1154 && x <= 1245 && y >= 502 && y <= 889)
+        {
+            System.out.println("Advertising!!");
+        }
+
+
     }
 }
