5baa12ad19668831c58a8942d76ab2be1fee0fc3
NS254 <senrenjp@gmail.com>
2024-10-21 09:10:22 -0400

    add floor works

--- src/game/entities/rooms/ResearchFloor.java
+++ src/game/entities/rooms/ResearchFloor.java
@@ -81,6 +81,7 @@ public class ResearchFloor extends Room{
             productLine.remove(productLine.getFirst());
             research = false;
         }
+
         else if (resTimer<=0 && productLine.isEmpty())
         {
             productsAvailable = false;
